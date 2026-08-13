89d8a4f91297b2fc17b14b388321164a7e5ed015
GustavoBrw <berwiang@gmail.com>
2026-08-12 21:56:24 -0300

    feat(infrastructure): adiciona DTO e mapper de evento

---
+++ src/main/java/dev/java10x/EventClean/infrastructure/dtos/EventoDto.java
@@ -0,0 +1,19 @@
+package dev.java10x.EventClean.infrastructure.dtos;
+
+import dev.java10x.EventClean.core.enuns.TipoEvento;
+
+import java.time.LocalDateTime;
+
+public record EventoDto(
+    Long id,
+    String nome,
+    String descricao,
+    String identificador,
+    LocalDateTime dataInico,
+    LocalDateTime dataFim,
+    String localEvento,
+    Integer capacidade,
+    String organizador,
+    TipoEvento tipo
+) {
+}
