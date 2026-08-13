c9bcb1baad4567e1684956df41684cf3f037d2c4
GustavoBrw <berwiang@gmail.com>
2026-08-12 21:56:06 -0300

    feat(core): implementa caso de uso de criação de evento

--- src/main/java/dev/java10x/EventClean/core/usecases/CriarEventoUseCaseImpl.java
+++ src/main/java/dev/java10x/EventClean/core/usecases/CriarEventoUseCaseImpl.java
@@ -2,7 +2,7 @@ package dev.java10x.EventClean.core.usecases;
 
 import dev.java10x.EventClean.core.entities.Evento;
 
-public class criarEventoCaseImpl implements criarEventoCase{
+public class CriarEventoUseCaseImpl implements CriarEventoUseCase {
 
     @Override
     public Evento execute(Evento evento) {
