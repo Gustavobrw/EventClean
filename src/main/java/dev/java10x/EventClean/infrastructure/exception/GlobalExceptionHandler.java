6a73c208da6141a0b4bd93927c57eaad74edca13
GustavoBrw <berwiang@gmail.com>
2026-09-20 15:51:09 -0300

    refactor: Melhora mensagem de retorno de erro

--- src/main/java/dev/java10x/EventClean/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/java10x/EventClean/infrastructure/exception/GlobalExceptionHandler.java
@@ -5,10 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(EventDuplicateException.class)
-    public ResponseEntity<String> handleEventDuplicateException(EventDuplicateException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<Map<String, String>> handleEventDuplicateException(EventDuplicateException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error: ", ex.getMessage());
+        response.put("message: ", "Evento com identificador duplicado.");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 }
